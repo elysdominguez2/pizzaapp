e05840cea6af3b5d346ae8c16a3ae3e9b084a62e
elysdominguez <elysdominguez2@gmail.com>
2022-09-13 22:38:21 +0200

    Adding favorites

--- src/components/PizzaList.js
+++ src/components/PizzaList.js
@@ -4,10 +4,14 @@ import { useSelector } from "react-redux"; //10)agregue esto
 import { selectUser } from "../store/user/selectors"; //10)agregue esto
 import { selectPizzas } from "../store/pizzas/selectors"; //13)Aca traigo el selector de las pizzas para poder imprimirlas y además contar cuantas hay para imprimir
 import AddPizzaForm from "./AddPizzaForm"; //16)importo el componente
+// import { BsHeartFill, BsHeart } from "react-icons/bs"; //21) ESTO AL FINA NO LO USE - Pero lee el comentario este  igual ->Instale react-icons y traje los corazones, abajo adentro de un boton lo puse a los iconos y hice una funcion que incluya el id de la pizza que me gusta, si me gusta corazon lleno, si no vacio <-- continua en 22) User - slice.js
+import { useDispatch } from "react-redux"; //23)Aca traigo el useDispatch y abajo traigo el state del toggleFavorites
+import { toggleFavorites } from "../store/user/slice"; //23)esto tambien me traje y hago una constante más abajo con el dispatch
 //9) y pongo lo siguiente
 // Ahora voy a la App.js y agrego el COMPONENTE PizzaList
 
 function PizzaList() {
+  const dispatch = useDispatch(); //23) Aqui la constante <-- continua más abajo en el boton con el paso 24)
   const user = useSelector(selectUser); //10)agregue esto
   const pizzas = useSelector(selectPizzas); //13)Creo una constante que traiga las pizzas y me las guarde en una constante que será un objeto que yo despues puedo separar en partes como pizzas.name, pizzas.description, etc
   return (
@@ -28,9 +32,18 @@ function PizzaList() {
         {pizzas.map((pizza) => (
           <li key={pizza.id}>
             <h4>{pizza.name}</h4>
+            <button
+              onClick={() => {
+                dispatch(toggleFavorites(pizza.id));
+              }}
+            >
+              {/* 24)Adentro de la apertura del boton tengo que poner un onClick que tenga una funcion que llame al dispatch, NO puedo poner solo dispatch ( siempre en arrow function porque si no tiene infinite loop) y adentro de esa funcion lo que yo ya cree en slice.js User y dentro de esa le pasamos lo que tiene que chequear, en este caso pizza.id para ver si es true or false <--sigue paso 25) en slice.js User */}
+              {user.favorites.includes(pizza.id) ? "♥" : "♡"}
+            </button>
+
             <p>{pizza.description}</p>
             <p>Bought: {pizza.bought} times!</p>
-            <img src={pizza.image} alt="" />
+            <img style={{ width: 200 }} src={pizza.image} alt="" />
           </li>
         ))}
       </ul>
--- src/store/user/slice.js
+++ src/store/user/slice.js
@@ -1,22 +1,40 @@
 //7) aca vamos a hardocrear porque no tenemos API pero en
 //otra ocacion puede que estemos llamando una API
 
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, current } from "@reduxjs/toolkit";
 
 //siempre comenzaremos con un initialState
 const initialState = {
   name: "Helva",
   id: 42,
-  favorites: [67283, 357311], //20)Agrego los favortos, aca los hardcoreo por ahora
+  favorites: [67283, 357311], //20)Agrego los favortos, aca los hardcoreo por ahora, <--- continua en 21) en PizzaList
 };
 
 export const userSlice = createSlice({
   name: "user",
   initialState,
-  reducers: {},
+  reducers: {
+    toggleFavorites: (state, action) => {
+      //25)Agrego aca el action y en el console.log de abajo para chequear que se estan imprimiendo cada vez que apreto el boton en la consola el mensaje junto con el action que me muestra el id.
+      //22)Agregando una new case al reducer e importandolo abajo, recordar que esto esta en el paso 11 de mis papeles <-- Sigue en paso 23) en PizzaList
+      console.log("Hello from userReducer", action); //25)este console.log chequea como explico arriba.
+      console.log("state", current(state)); //26)si quiero hacer console.log del STATE debo poner current(state) e importarlo arriba como se ve junto con el createSlice, esto es para chequear que hay dentro del state
+      //26)Ahora creo la lógica para agregar o sacar el corazon de favorito aca abajo y LEE porque esta comentado el newFavoriteArray en la misma funcio comentada
+      const id = action.payload;
+
+      // const newFavoritesArray = state.user.favorites.includes(id) ? state.user.favorites.filter(favoriteId => favoriteId !== id) : [...state.user.favorites, id];//26)Esta la comenté para que me de cuenta que NO hay que usar .user(comparar las dos constantes, una lo tiene y la otra no, YA estoy dentro de USER por eso no lo llamo, eso lo hago en los SELECTORS porque estoy fuera del state)
+      const newFavoritesArray = state.favorites.includes(id)
+        ? state.favorites.filter((favoriteId) => favoriteId !== id)
+        : [...state.favorites, id];
+
+      console.log("new Array", newFavoritesArray);
+
+      state.favorites = newFavoritesArray; //27) Finalmente agrego esta linea para hacer update de los datos y puedo ver como funcionan los botones
+    },
+  },
 });
 
-export const {} = userSlice.actions;
+export const { toggleFavorites } = userSlice.actions;
 
 export default userSlice.reducer;
 
